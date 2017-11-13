2857c397521070d1c048dab7d920701efc98dabe
Ying <yizheng@google.com>
2017-11-13 10:36:59 -0800

    Refactor User page to Users&Accounts page for car settings, display a list of accounts owned by the current user.
    A couple things not working yet:
    1) Icon for account type not working, b/69257514
    2) 3rd party app account not showing up, b/69256424
    
    Bug: 69167510
    Test: Run through settings flow
    Change-Id: Ie678d4028c9a2fdbff11b71500d7be5121cac94f

---
+++ src/com/android/car/settings/accounts/AccountDetailsFragment.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.car.settings.accounts;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.car.list.EditTextLineItem;
+import com.android.car.list.SingleTextLineItem;
+import com.android.car.list.TextLineItem;
+import com.android.car.list.TypedPagedListAdapter;
+import com.android.car.settings.R;
+import com.android.car.settings.common.ListSettingsFragment;
+import com.android.car.settings.users.UserDetailsSettingsFragment;
+
+import java.util.ArrayList;
+
+/**
+ * Shows account details, and delete account option.
+ */
+public class AccountDetailsFragment extends ListSettingsFragment {
+    private static final String TAG = "AccountDetailsFragment";
+
+    public static final String EXTRA_ACCOUNT_INFO = "extra_account_info";
+
+    private Account mAccount;
+    private AccountManager mAccountManager;
+
+    public static AccountDetailsFragment getInstance(Account account) {
+        AccountDetailsFragment
+                accountDetailsFragment = new AccountDetailsFragment();
+        Bundle bundle = ListSettingsFragment.getBundle();
+        bundle.putInt(EXTRA_ACTION_BAR_LAYOUT, R.layout.action_bar_with_button);
+        bundle.putInt(EXTRA_TITLE_ID, R.string.account_details_title);
+        bundle.putParcelable(EXTRA_ACCOUNT_INFO, account);
+        accountDetailsFragment.setArguments(bundle);
+        return accountDetailsFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAccount = getArguments().getParcelable(EXTRA_ACCOUNT_INFO);
+    }
+
+    @Override
+    public ArrayList<TypedPagedListAdapter.LineItem> getLineItems() {
+        ArrayList<TypedPagedListAdapter.LineItem> lineItems = new ArrayList<>();
+        lineItems.add(new SingleTextLineItem(mAccount.name));
+        return lineItems;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mAccountManager = AccountManager.get(getActivity());
+        TextView removeAccountBtn = getActivity().findViewById(R.id.action_button1);
+        removeAccountBtn.setText(R.string.delete_button);
+        removeAccountBtn.setOnClickListener(v -> removeAccount());
+    }
+
+    private void removeAccount() {
+        // TODO Switch to removeAccountAsUser
+        if (mAccountManager.removeAccountExplicitly(mAccount)) {
+            getActivity().onBackPressed();
+        }
+    }
+}
