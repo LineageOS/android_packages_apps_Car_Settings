a12477f8cb8f3d41fb625bca67fd62c6bd4e8688
Lujiang Xue <rogerxue@google.com>
2018-04-10 08:20:29 -0700

    make quick setting distraction optimized
    Bug: 76805265
    
    Test: manual
    Change-Id: I0e1336c75ecb47a14a550c8bf0fe6a4880c2883c

--- src/com/android/car/settings/quicksettings/QuickSettingActivity.java
+++ src/com/android/car/settings/quicksettings/QuickSettingActivity.java
@@ -15,35 +15,77 @@
  */
 package com.android.car.settings.quicksettings;
 
+import android.app.Dialog;
+import android.car.drivingstate.CarUxRestrictions;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.UserInfo;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.car.app.CarAlertDialog;
 import androidx.car.widget.PagedListView;
 
 import com.android.car.settings.R;
 import com.android.car.settings.common.CarSettingActivity;
+import com.android.car.settings.common.CarUxRestrictionsHelper;
+import com.android.car.settings.users.UserIconProvider;
+import com.android.settingslib.users.UserManagerHelper;
 
 /**
  * Shows a page to access frequently used settings.
  */
 public class QuickSettingActivity extends AppCompatActivity {
     private static final String TAG = "QS";
+    private static final String DIALOG_TAG = "block_dialog_tag";
 
+    private static final float RESTRICTED_ALPHA = 0.5f;
+    private static final float UNRESTRICTED_ALPHA = 1f;
+
+    private CarUxRestrictionsHelper mUxRestrictionsHelper;
+    private UserManagerHelper  mUserManagerHelper;
     private QuickSettingGridAdapter mGridAdapter;
     private PagedListView mListView;
+    private View mFullSettingBtn;
+    private View mUserSwitcherBtn;
 
+    private final OnClickListener mLaunchSettingListener = v -> {
+        Intent intent = new Intent(this, CarSettingActivity.class);
+        startActivity(intent);
+    };
+
+    private final OnClickListener mBlockingListener = v -> {
+        AlertDialogFragment alertDialog = new AlertDialogFragment();
+        alertDialog.show(getSupportFragmentManager(), DIALOG_TAG);
+    };
+
+    /**
+     * Shows a dialog to notify user that the actions is not available while driving.
+     */
+    public static class AlertDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Context context = getContext();
+            return new CarAlertDialog.Builder(context)
+                    .setBody(context.getString(R.string.restricted_while_driving))
+                    .setPositiveButton(context.getString(R.string.okay),
+                            /* listener= */ null)
+                    .create();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mUserManagerHelper = new UserManagerHelper(this);
         setContentView(R.layout.quick_settings);
-
         mListView = (PagedListView) findViewById(R.id.list);
         mGridAdapter = new QuickSettingGridAdapter(this);
         mListView.getRecyclerView().setLayoutManager(mGridAdapter.getGridLayoutManager());
@@ -53,28 +95,33 @@ public class QuickSettingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(false);
-        actionBar.setCustomView(R.layout.action_bar_with_button);
+        actionBar.setCustomView(R.layout.action_bar_quick_settings);
         actionBar.setDisplayShowCustomEnabled(true);
 
-        Button adavancedSettingBtn = (Button) findViewById(R.id.action_button1);
-        Button userSwitcherBtn = (Button) findViewById(R.id.action_button2);
-        adavancedSettingBtn.setText(R.string.advanced_settings_label);
-        adavancedSettingBtn.setVisibility(View.VISIBLE);
-        adavancedSettingBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(this, CarSettingActivity.class);
-            startActivity(intent);
-        });
-
-        userSwitcherBtn.setText(R.string.user_and_account_settings_title);
-        userSwitcherBtn.setVisibility(View.VISIBLE);
-        userSwitcherBtn.setOnClickListener(v -> {
+        mFullSettingBtn = findViewById(R.id.full_setting_btn);
+        mUserSwitcherBtn = findViewById(R.id.user_switcher_btn);
+        mUserSwitcherBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, CarSettingActivity.class);
             intent.setAction(CarSettingActivity.ACTION_LIST_USER);
             startActivity(intent);
         });
-        View exitBtn = findViewById(R.id.back_button);
-        ((ImageView) exitBtn).setImageResource(R.drawable.ic_close);
+
+        setupAccountButton();
+        View exitBtn = findViewById(R.id.exit_button);
         exitBtn.setOnClickListener(v -> finish());
+        mUxRestrictionsHelper =
+                new CarUxRestrictionsHelper(
+                        this, QuickSettingActivity.this::onUxRestrictionChagned);
+    }
+
+    private void setupAccountButton() {
+        ImageView userIcon = (ImageView) findViewById(R.id.user_icon);
+        UserInfo currentUserInfo = mUserManagerHelper.getForegroundUserInfo();
+        userIcon.setImageDrawable(
+                UserIconProvider.getUserIcon(currentUserInfo, mUserManagerHelper, this));
+
+        TextView userSwitcherText = (TextView) findViewById(R.id.user_switcher_text);
+        userSwitcherText.setText(currentUserInfo.name);
     }
 
     @Override
@@ -87,11 +134,31 @@ public class QuickSettingActivity extends AppCompatActivity {
                 .addTile(new DayNightTile(this, mGridAdapter))
                 .addSeekbarTile(new BrightnessTile(this));
         mListView.setAdapter(mGridAdapter);
+        mUxRestrictionsHelper.start();
     }
 
     @Override
     public void onStop() {
         super.onStop();
         mGridAdapter.stop();
+        mUxRestrictionsHelper.stop();
+    }
+
+    private void onUxRestrictionChagned(CarUxRestrictions carUxRestrictions) {
+        // TODO: update tiles
+        applyRestriction(
+                (carUxRestrictions.getActiveRestrictions()
+                        & CarUxRestrictions.UX_RESTRICTIONS_NO_SETUP)
+                        == CarUxRestrictions.UX_RESTRICTIONS_NO_SETUP);
+    }
+
+    private void applyRestriction(boolean restricted) {
+        if (restricted) {
+            mFullSettingBtn.setAlpha(RESTRICTED_ALPHA);
+            mFullSettingBtn.setOnClickListener(mBlockingListener);
+        } else {
+            mFullSettingBtn.setAlpha(UNRESTRICTED_ALPHA);
+            mFullSettingBtn.setOnClickListener(mLaunchSettingListener);
+        }
     }
 }
