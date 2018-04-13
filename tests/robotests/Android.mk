c9623a1ccfe504bdbe243de62bc7a40c5a00d82e
Anton Hansson <hansson@google.com>
2018-04-13 15:34:54 +0100

    Car/Settings: Remove direct dep on sdk_vcurrent module.
    Doesn't appear to be needed. If it was needed, the right way to depend on
    this module is to specify LOCAL_SDK_VERSION. This particular module is
    getting deleted soon.
    
    Note that this test fails with and without my change.
    
    Test: m -j RunCarSettingsRoboTests has no new failures
    Bug: 77525052
    Change-Id: I1a309cc29b04028ff38717ceb3503511a5a42d80
    Merged-In: Id303080da52acd042201a490a5f7dade6a866d1e

--- tests/robotests/Android.mk
+++ tests/robotests/Android.mk
@@ -13,8 +13,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_JAVA_LIBRARIES := \
     junit \
-    platform-robolectric-prebuilt \
-    sdk_vcurrent
+    platform-robolectric-prebuilt
 
 LOCAL_INSTRUMENTATION_FOR := CarSettings
 LOCAL_MODULE := CarSettingsRoboTests
