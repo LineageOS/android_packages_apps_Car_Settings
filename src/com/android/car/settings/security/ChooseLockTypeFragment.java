b5b11b7d1400107d576b7905aa91edd2c370bca4
Jian Jin <jinjian@google.com>
2018-04-27 15:31:51 -0700

    DO NOT MERGE - Add None option to clear any existing lock
    - Display "Current screen lock" subtitle in the lock type list
    - Show confirmation dialog when None is selected
    - Clear the lock when user clicks on Remove in the dialog
    
    Fixes: 77639190
    Test: RunCarSettingsRoboTests
    Change-Id: I4c55369183b897d25e756f518ab6272003e447c8

--- src/com/android/car/settings/security/ChooseLockTypeFragment.java
+++ src/com/android/car/settings/security/ChooseLockTypeFragment.java
@@ -16,7 +16,9 @@
 
 package com.android.car.settings.security;
 
+import android.app.admin.DevicePolicyManager;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.text.TextUtils;
 
 import androidx.car.widget.ListItem;
@@ -26,6 +28,7 @@ import androidx.car.widget.TextListItem;
 import com.android.car.settings.R;
 import com.android.car.settings.common.BaseFragment;
 import com.android.car.settings.common.ListItemSettingsFragment;
+import com.android.internal.widget.LockPatternUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +37,19 @@ import java.util.List;
  * Give user choices of lock screen type: Pin/Pattern/Password or None.
  */
 public class ChooseLockTypeFragment extends ListItemSettingsFragment {
+
+    public static final String EXTRA_CURRENT_PASSWORD_QUALITY = "extra_current_password_quality";
+    private static final String DIALOG_TAG = "ConfirmRemoveScreenLockDialog";
+
     private ListItemProvider mItemProvider;
     private String mCurrPassword;
+    private int mPasswordQuality;
+
+    private final ConfirmRemoveScreenLockDialog.ConfirmRemoveLockListener mConfirmListener = () -> {
+        int userId = UserHandle.myUserId();
+        new LockPatternUtils(getContext()).clearLock(mCurrPassword, userId);
+        getFragmentController().goBack();
+    };
 
     public static ChooseLockTypeFragment newInstance() {
         ChooseLockTypeFragment chooseLockTypeFragment = new ChooseLockTypeFragment();
@@ -52,6 +66,15 @@ public class ChooseLockTypeFragment extends ListItemSettingsFragment {
         Bundle args = getArguments();
         if (args != null) {
             mCurrPassword = args.getString(SettingsScreenLockActivity.EXTRA_CURRENT_SCREEN_LOCK);
+            mPasswordQuality = args.getInt(EXTRA_CURRENT_PASSWORD_QUALITY);
+        }
+
+        if (savedInstanceState != null) {
+            ConfirmRemoveScreenLockDialog dialog = (ConfirmRemoveScreenLockDialog)
+                    getFragmentManager().findFragmentByTag(DIALOG_TAG);
+            if (dialog != null) {
+                dialog.setConfirmRemoveLockListener(mConfirmListener);
+            }
         }
     }
 
@@ -65,15 +88,35 @@ public class ChooseLockTypeFragment extends ListItemSettingsFragment {
 
     private List<ListItem> getListItems() {
         List<ListItem> items = new ArrayList<>();
+        items.add(createNoneLineItem());
         items.add(createLockPatternLineItem());
         items.add(createLockPasswordLineItem());
         items.add(createLockPinLineItem());
         return items;
     }
 
+    private ListItem createNoneLineItem() {
+        TextListItem item = new TextListItem(getContext());
+        item.setTitle(getString(R.string.security_lock_none));
+        if (mPasswordQuality == DevicePolicyManager.PASSWORD_QUALITY_UNSPECIFIED) {
+            item.setBody(getString(R.string.current_screen_lock));
+            // TODO set item disabled after b/78784323 is fixed
+        } else {
+            item.setOnClickListener(view -> {
+                ConfirmRemoveScreenLockDialog dialog = new ConfirmRemoveScreenLockDialog();
+                dialog.setConfirmRemoveLockListener(mConfirmListener);
+                dialog.show(getFragmentManager(), DIALOG_TAG);
+            });
+        }
+        return item;
+    }
+
     private ListItem createLockPatternLineItem() {
         TextListItem item = new TextListItem(getContext());
         item.setTitle(getString(R.string.security_lock_pattern));
+        if (mPasswordQuality == DevicePolicyManager.PASSWORD_QUALITY_SOMETHING) {
+            item.setBody(getString(R.string.current_screen_lock));
+        }
         item.setOnClickListener(view -> launchFragment(ChooseLockPatternFragment.newInstance()));
         return item;
     }
@@ -81,6 +124,10 @@ public class ChooseLockTypeFragment extends ListItemSettingsFragment {
     private ListItem createLockPasswordLineItem() {
         TextListItem item = new TextListItem(getContext());
         item.setTitle(getString(R.string.security_lock_password));
+        if (mPasswordQuality == DevicePolicyManager.PASSWORD_QUALITY_ALPHABETIC
+                || mPasswordQuality == DevicePolicyManager.PASSWORD_QUALITY_ALPHANUMERIC) {
+            item.setBody(getString(R.string.current_screen_lock));
+        }
         item.setOnClickListener(view -> launchFragment(
                 ChooseLockPinPasswordFragment.newPasswordInstance()));
         return item;
@@ -89,6 +136,10 @@ public class ChooseLockTypeFragment extends ListItemSettingsFragment {
     private ListItem createLockPinLineItem() {
         TextListItem item = new TextListItem(getContext());
         item.setTitle(getString(R.string.security_lock_pin));
+        if (mPasswordQuality == DevicePolicyManager.PASSWORD_QUALITY_NUMERIC
+                || mPasswordQuality == DevicePolicyManager.PASSWORD_QUALITY_NUMERIC_COMPLEX) {
+            item.setBody(getString(R.string.current_screen_lock));
+        }
         item.setOnClickListener(view -> launchFragment(
                 ChooseLockPinPasswordFragment.newPinInstance()));
         return item;
