5e4ff3fbbaa9061d273ecc72d91015869f6c5397
Lujiang Xue <rogerxue@google.com>
2018-05-23 15:16:10 -0700

    Let Fragment handle UXR change/blocking by - make activity UxRAware - have fragment check UxR on attach
    remaining issue:
    when dialog is showing, if restriction gets lifted, does not transition to current fragment automatically.
    
    Bug:80194649,80192342
    Test: on head unit
    Change-Id: I872766d2125fa0f9e8369a52f0ca2bd6c5abc469

--- src/com/android/car/settings/common/CarSettingActivity.java
+++ src/com/android/car/settings/common/CarSettingActivity.java
@@ -24,6 +24,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.inputmethod.InputMethodManager;
 
 import com.android.car.settings.R;
+import com.android.car.settings.common.BaseFragment.UXRestrictionsProvider;
 import com.android.car.settings.quicksettings.QuickSettingFragment;
 
 /**
@@ -31,9 +32,14 @@ import com.android.car.settings.quicksettings.QuickSettingFragment;
  * previous activity.
  */
 public class CarSettingActivity extends AppCompatActivity implements
-        BaseFragment.FragmentController {
+        BaseFragment.FragmentController, UXRestrictionsProvider {
     private CarUxRestrictionsHelper mUxRestrictionsHelper;
-    private CarUxRestrictions mCarUxRestrictions;
+    // Default to minimum restriction.
+    private CarUxRestrictions mCarUxRestrictions = new CarUxRestrictions.Builder(
+            /* reqOpt= */ true,
+            CarUxRestrictions.UX_RESTRICTIONS_BASELINE,
+            /* timestamp= */ 0
+    ).build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +47,23 @@ public class CarSettingActivity extends AppCompatActivity implements
         setContentView(R.layout.app_compat_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
         if (mUxRestrictionsHelper == null) {
             mUxRestrictionsHelper =
                     new CarUxRestrictionsHelper(this, carUxRestrictions -> {
                         mCarUxRestrictions = carUxRestrictions;
                         BaseFragment currentFragment = getCurrentFragment();
                         if (currentFragment != null) {
-                            currentFragment.setCarUxRestrictions(carUxRestrictions);
+                            currentFragment.onUxRestrictionChanged(carUxRestrictions);
                         }
                     });
         }
 
         mUxRestrictionsHelper.start();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
 
         if (getCurrentFragment() == null) {
             launchFragment(QuickSettingFragment.newInstance());
@@ -65,9 +71,21 @@ public class CarSettingActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public CarUxRestrictions getCarUxRestrictions() {
+        return mCarUxRestrictions;
+    }
+
+    @Override
+    public void notifyCurrentFragmentRestricted() {
+        DOBlockingDialogFragment alertDialog = new DOBlockingDialogFragment();
+        alertDialog.show(getSupportFragmentManager(), DOBlockingDialogFragment.DIALOG_TAG);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
         mUxRestrictionsHelper.stop();
+        mUxRestrictionsHelper = null;
     }
 
     @Override
@@ -77,14 +95,6 @@ public class CarSettingActivity extends AppCompatActivity implements
 
     @Override
     public void launchFragment(BaseFragment fragment) {
-        if (mCarUxRestrictions != null && !fragment.canBeShown(mCarUxRestrictions)) {
-            DoBlockingDialogFragment alertDialog = new DoBlockingDialogFragment();
-            alertDialog.show(getSupportFragmentManager(), DoBlockingDialogFragment.DIALOG_TAG);
-            return;
-        }
-        if (mCarUxRestrictions != null) {
-            fragment.setCarUxRestrictions(mCarUxRestrictions);
-        }
         getSupportFragmentManager()
                 .beginTransaction()
                 .setCustomAnimations(
