33c05015caf69714dd22c3dea917400252fc595a
davidln <davidln@google.com>
2018-09-21 17:25:03 -0700

    Add base class for preference controllers.
    Preference controllers will handle the business logic of preference settings. Some classes referenced in the documentation will be added in immediate follow-ups. This is a manageable piece of a larger full feature impl.
    
    Bug: 112931076
    
    Test: end to end test in full feature change, make RunCarSettingsRoboTests
    Change-Id: Ibb42c65efa7444af6ba805bd1a7ab4230350f244

---
+++ tests/robotests/src/com/android/car/settings/common/FakePreferenceController.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.settings.common;
+
+import android.content.Context;
+
+/**
+ * Concrete {@link BasePreferenceController} with an accessible {@link AvailabilityStatus}.
+ */
+public class FakePreferenceController extends BasePreferenceController {
+
+    @AvailabilityStatus
+    private int mAvailabilityStatus = AVAILABLE;
+
+    public FakePreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    @AvailabilityStatus
+    public int getAvailabilityStatus() {
+        return mAvailabilityStatus;
+    }
+
+    public void setAvailabilityStatus(@AvailabilityStatus int availabilityStatus) {
+        mAvailabilityStatus = availabilityStatus;
+    }
+}
