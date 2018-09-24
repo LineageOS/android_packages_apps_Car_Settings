bace9881f9cc83c605f13490c26404f6deb89d6f
davidln <davidln@google.com>
2018-09-24 10:12:20 -0700

    Add helper class for generating a list of preference controllers parsed from XML.
    Bug: 112931076
    
    Test: end to end in full feature CL, RunCarSettingsRoboTests
    Change-Id: I48e5b978f3d665db01bd98bb699596090a5f4856

---
+++ tests/robotests/src/com/android/car/settings/common/PreferenceControllerListHelperTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.settings.common;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.android.car.settings.CarSettingsRobolectricTestRunner;
+import com.android.car.settings.R;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RuntimeEnvironment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/** Unit test for {@link PreferenceControllerListHelper}. */
+@RunWith(CarSettingsRobolectricTestRunner.class)
+public class PreferenceControllerListHelperTest {
+
+    @Test
+    public void getControllers_returnsList() {
+        List<String> validKeys = Arrays.asList("key1", "key2");
+        List<BasePreferenceController> controllers =
+                PreferenceControllerListHelper.getPreferenceControllersFromXml(
+                        RuntimeEnvironment.application, R.xml.preference_controller_list_helper);
+
+        assertThat(controllers).hasSize(validKeys.size());
+        List<String> foundKeys = new ArrayList<>();
+        for (BasePreferenceController controller : controllers) {
+            assertThat(controller).isInstanceOf(FakePreferenceController.class);
+            foundKeys.add(controller.getPreferenceKey());
+        }
+        assertThat(foundKeys).containsAllIn(validKeys);
+    }
+}
