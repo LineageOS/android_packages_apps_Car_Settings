3565cf0260230b8cdfa2fa176ce3220ccc857b43
Heemin Seog <hseog@google.com>
2018-10-25 11:35:48 -0700

    Migrate Display Settings to Preferences.
    Display settings can be represented with SwitchPreference and
    SeekBarPreference. Note that SeekBarPreference looks slightly different
    than what originally existed (seek bar is not aligned with text), but
    this can be changed via changing the preference styles in the future.
    
    Also, actual testing of the change of the display brightness was not
    observed on the Mojave, because there isn't a correct LightsService
    built to handle it. Still, all of the logic remains the same as before.
    
    We include our own version of SeekBarPreference due to the inflexibility
    of the standard SeekBarPreference. We add a functionality to send
    continuous updates to the listener while changing the seek bar.
    
    Bug: 117336555
    Test: Build, Manual, Robolectric unit tests
    Change-Id: I4c1f96fcfc414ca77c1addec64ab76a4c5e434d2

---
+++ src/com/android/car/settings/display/AdaptiveBrightnessTogglePreferenceController.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.settings.display;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.TwoStatePreference;
+
+import com.android.car.settings.common.FragmentController;
+import com.android.car.settings.common.NoSetupPreferenceController;
+
+/** Business logic for controlling the adaptive brightness setting. */
+public class AdaptiveBrightnessTogglePreferenceController extends
+        NoSetupPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    public AdaptiveBrightnessTogglePreferenceController(Context context, String preferenceKey,
+            FragmentController fragmentController) {
+        super(context, preferenceKey, fragmentController);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        if (!(preference instanceof TwoStatePreference)) {
+            throw new IllegalArgumentException("Expecting an instance of TwoStatePreference");
+        }
+        ((TwoStatePreference) preference).setChecked(isAdaptiveBrightnessChecked());
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return supportsAdaptiveBrightness() ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!(preference instanceof TwoStatePreference)) {
+            throw new IllegalArgumentException("Expecting an instance of TwoStatePreference");
+        }
+        boolean enableAdaptiveBrightness = (boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                enableAdaptiveBrightness ? Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC
+                        : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        return true;
+    }
+
+    private boolean isAdaptiveBrightnessChecked() {
+        int brightnessMode = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        return brightnessMode != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+    }
+
+    private boolean supportsAdaptiveBrightness() {
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+    }
+}
