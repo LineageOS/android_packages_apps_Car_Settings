f402bff1e3297d173657993dd31366481423571e
Lujiang Xue <rogerxue@google.com>
2018-12-19 16:02:39 -0800

    Adding more action filters and map them to cooresponding fragment
    Bug: 73470808
    
    Test: tested on head unit
    Change-Id: I9b8263f5b5d355b653925430457465734f20642b

--- src/com/android/car/settings/common/FragmentResolver.java
+++ src/com/android/car/settings/common/FragmentResolver.java
@@ -20,7 +20,13 @@ import android.provider.Settings;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.car.settings.applications.ApplicationsSettingsFragment;
+import com.android.car.settings.bluetooth.BluetoothSettingsFragment;
+import com.android.car.settings.datetime.DatetimeSettingsFragment;
+import com.android.car.settings.display.DisplaySettingsFragment;
 import com.android.car.settings.location.LocationSettingsFragment;
+import com.android.car.settings.sound.SoundSettingsFragment;
+import com.android.car.settings.users.UsersListFragment;
 import com.android.car.settings.wifi.WifiSettingsFragment;
 
 
@@ -38,9 +44,33 @@ public class FragmentResolver {
         switch (action) {
             case Settings.ACTION_LOCATION_SOURCE_SETTINGS:
                 return new LocationSettingsFragment();
+
             case android.net.wifi.WifiManager.ACTION_PICK_WIFI_NETWORK:
             case Settings.ACTION_WIFI_SETTINGS:
+            case Settings.ACTION_WIRELESS_SETTINGS:
                 return new WifiSettingsFragment();
+
+            case Settings.ACTION_USER_SETTINGS:
+            case Settings.ACTION_SECURITY_SETTINGS:
+                return new UsersListFragment();
+
+            case Settings.ACTION_BLUETOOTH_SETTINGS:
+                return new BluetoothSettingsFragment();
+
+            case Settings.ACTION_DATE_SETTINGS:
+                return new DatetimeSettingsFragment();
+
+            case Settings.ACTION_SOUND_SETTINGS:
+                return new SoundSettingsFragment();
+
+            case Settings.ACTION_DISPLAY_SETTINGS:
+            case Settings.ACTION_NIGHT_DISPLAY_SETTINGS:
+                return new DisplaySettingsFragment();
+
+            case Settings.ACTION_APPLICATION_SETTINGS:
+            case Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS:
+            case Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS:
+                return new ApplicationsSettingsFragment();
             default:
                 return null;
         }
