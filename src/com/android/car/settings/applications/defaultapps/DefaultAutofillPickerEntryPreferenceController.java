39c77d68aa6efe63647247cd22d764ed5b833d63
Heemin Seog <hseog@google.com>
2019-02-01 08:40:03 -0800

    DO NOT MERGE Autofill picker to select default autofill service
    Note that we differ slightly from the phone, since it doesn't quite make
    sense to have an option to "Add service" yet.
    
    Bug: 122824177
    Test: Manual (install Keeper/LastPass from play store), Robolectric
    Change-Id: Ibac27b6c3aef06ab0ff5ae3a0e231a08a1514824

--- src/com/android/car/settings/applications/defaultapps/DefaultAutofillPickerEntryPreferenceController.java
+++ src/com/android/car/settings/applications/defaultapps/DefaultAutofillPickerEntryPreferenceController.java
@@ -39,14 +39,15 @@ import com.android.settingslib.wrapper.PackageManagerWrapper;
 import java.util.List;
 
 /** Business logic for displaying the currently selected autofill app. */
-public class AutofillPickerEntryPreferenceController extends
+public class DefaultAutofillPickerEntryPreferenceController extends
         DefaultAppsPickerEntryBasePreferenceController {
 
-    private static final Logger LOG = new Logger(AutofillPickerEntryPreferenceController.class);
+    private static final Logger LOG = new Logger(
+            DefaultAutofillPickerEntryPreferenceController.class);
     private final AutofillManager mAutofillManager;
     private final PackageManagerWrapper mPm;
 
-    public AutofillPickerEntryPreferenceController(Context context, String preferenceKey,
+    public DefaultAutofillPickerEntryPreferenceController(Context context, String preferenceKey,
             FragmentController fragmentController, CarUxRestrictions uxRestrictions) {
         super(context, preferenceKey, fragmentController, uxRestrictions);
         mAutofillManager = context.getSystemService(AutofillManager.class);
