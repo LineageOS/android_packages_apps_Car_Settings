0ecdc036a27a857ffdb2f1f1877adad2f1a93011
Abhijoy Saha <abhijoy@google.com>
2019-05-07 16:49:18 -0700

    DO NOT MERGE Tests for KeyboardManagementPreferenceController
    Add tests for KeyboardManagementPreferenceController and its helper
    functions in InputMethodUtil. This required changes in
    implementation of ShadowInputMethodManager, hence, requiring a bit
    of additional changes to KeyboardPreferenceControllerTest and
    EnabledKeyboardPreferenceControllerTest.
    
    Bug: 123957435
    Test: Roboelectric
    Change-Id: I148f0e35fd0addad5a6a52a75a88f5acae1109c2

--- tests/robotests/src/com/android/car/settings/testutils/ShadowInputMethodManager.java
+++ tests/robotests/src/com/android/car/settings/testutils/ShadowInputMethodManager.java
@@ -16,29 +16,56 @@
 
 package com.android.car.settings.testutils;
 
+import android.provider.Settings;
 import android.view.inputmethod.InputMethodInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.view.inputmethod.InputMethodSubtype;
 
 import androidx.annotation.Nullable;
 
+import com.android.car.settings.inputmethod.InputMethodUtil;
+
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Implements(value = InputMethodManager.class)
 public class ShadowInputMethodManager extends org.robolectric.shadows.ShadowInputMethodManager {
-    private List<InputMethodInfo> mEnabledInputMethodList;
+    private List<InputMethodInfo> mInputMethodList;
+    private Map<String, InputMethodInfo> mInputMethodMap;
     private List<InputMethodSubtype> mInputMethodSubtypes;
 
-    public void setEnabledInputMethodList(@Nullable List<InputMethodInfo> list) {
-        mEnabledInputMethodList = list;
+    public void setEnabledInputMethodList(@Nullable List<InputMethodInfo> inputMethodInfos) {
+        String concatenatedInputMethodIds = createInputMethodIdString(inputMethodInfos.stream().map(
+                imi -> imi.getId()).collect(Collectors.toList()).toArray(
+                new String[inputMethodInfos.size()]));
+        Settings.Secure.putString(RuntimeEnvironment.application.getContentResolver(),
+                Settings.Secure.ENABLED_INPUT_METHODS, concatenatedInputMethodIds);
+        addInputMethodInfosToMap(inputMethodInfos);
     }
 
     @Implementation
-    public List<InputMethodInfo> getEnabledInputMethodList() {
-        return mEnabledInputMethodList;
+    protected List<InputMethodInfo> getEnabledInputMethodList() {
+        List<InputMethodInfo> enabledInputMethodList = new ArrayList<>();
+
+        String inputMethodIdString = Settings.Secure.getString(
+                RuntimeEnvironment.application.getContentResolver(),
+                Settings.Secure.ENABLED_INPUT_METHODS);
+        if (inputMethodIdString == null || inputMethodIdString.isEmpty()) {
+            return enabledInputMethodList;
+        }
+
+        InputMethodUtil.sInputMethodSplitter.setString(inputMethodIdString);
+        while (InputMethodUtil.sInputMethodSplitter.hasNext()) {
+            enabledInputMethodList.add(mInputMethodMap.get(InputMethodUtil.sInputMethodSplitter
+                    .next()));
+        }
+        return enabledInputMethodList;
     }
 
     public void setEnabledInputMethodSubtypeList(List<InputMethodSubtype> list) {
@@ -46,8 +73,51 @@ public class ShadowInputMethodManager extends org.robolectric.shadows.ShadowInpu
     }
 
     @Implementation
-    public List<InputMethodSubtype> getEnabledInputMethodSubtypeList(InputMethodInfo imi,
+    protected List<InputMethodSubtype> getEnabledInputMethodSubtypeList(InputMethodInfo imi,
             boolean allowsImplicitlySelectedSubtypes) {
         return mInputMethodSubtypes;
+    }
+
+    public void setInputMethodList(List<InputMethodInfo> inputMethodInfos) {
+        mInputMethodList = inputMethodInfos;
+        if (inputMethodInfos == null) {
+            return;
+        }
+
+        addInputMethodInfosToMap(inputMethodInfos);
+    }
+
+    @Implementation
+    protected List<InputMethodInfo> getInputMethodList() {
+        return mInputMethodList;
+    }
+
+    private static String createInputMethodIdString(String... ids) {
+        int size = ids == null ? 0 : ids.length;
+
+        if (size == 1) {
+            return ids[0];
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            builder.append(ids[i]);
+            if (i != size - 1) {
+                builder.append(InputMethodUtil.INPUT_METHOD_DELIMITER);
+            }
+        }
+        return builder.toString();
+    }
+
+    private void addInputMethodInfosToMap(List<InputMethodInfo> inputMethodInfos) {
+        if (mInputMethodMap == null || mInputMethodMap.size() == 0) {
+            mInputMethodMap = inputMethodInfos.stream().collect(Collectors.toMap(
+                    InputMethodInfo::getId, imi -> imi));
+            return;
+        }
+
+        inputMethodInfos.forEach(imi -> {
+            mInputMethodMap.put(imi.getId(), imi);
+        });
     }
 }
