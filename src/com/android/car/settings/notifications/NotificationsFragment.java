0599a65be1ddff09b0131d0701d01f216b46b093
eschiang <eschiang@google.com>
2021-05-22 00:08:01 +0000

    Implement notifications screen
    Added notifications screen with recent apps and all apps sections.
    
    Bug: 181715470
    Test: Made sure toggle button changes were reflected in both sections if an app showed in both. Made sure toggle button changes were reflected in the app details page as well. Ran atest CarSettingsUnitTests
    Change-Id: Ia5937e779cceb370e4fe89eddcf9395a88738b06

--- src/com/android/car/settings/notifications/NotificationsFragment.java
+++ src/com/android/car/settings/notifications/NotificationsFragment.java
@@ -16,22 +16,100 @@
 
 package com.android.car.settings.notifications;
 
+import static com.android.car.settings.storage.StorageUtils.maybeInitializeVolume;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.storage.StorageManager;
+import android.os.storage.VolumeInfo;
 import android.provider.Settings;
 
 import androidx.annotation.XmlRes;
 
 import com.android.car.settings.R;
+import com.android.car.settings.applications.ApplicationListItemManager;
 import com.android.car.settings.common.SettingsFragment;
 import com.android.car.settings.search.CarBaseSearchIndexProvider;
+import com.android.settingslib.applications.ApplicationsState;
 import com.android.settingslib.search.SearchIndexable;
 
 /** Shows subsettings related to notifications. */
 @SearchIndexable
 public class NotificationsFragment extends SettingsFragment {
+
+    private ApplicationListItemManager mAppListItemManager;
     @Override
     @XmlRes
     protected int getPreferenceScreenResId() {
         return R.xml.notifications_fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        Application application = requireActivity().getApplication();
+        ApplicationsState applicationsState = ApplicationsState.getInstance(application);
+        StorageManager sm = context.getSystemService(StorageManager.class);
+        VolumeInfo volume = maybeInitializeVolume(sm, getArguments());
+
+        NotificationsAppListPreferenceController notificationsAppListController =
+                use(NotificationsAppListPreferenceController.class,
+                        R.string.pk_notifications_settings_all_apps);
+        RecentNotificationsAppsPreferenceController recentNotificationsController =
+                use(RecentNotificationsAppsPreferenceController.class,
+                        R.string.pk_notifications_settings_recently_sent);
+
+        mAppListItemManager = new ApplicationListItemManager(volume, getLifecycle(),
+                applicationsState,
+                getContext().getResources().getInteger(
+                        R.integer.millisecond_app_data_update_interval),
+                getContext().getResources().getInteger(
+                        R.integer.millisecond_max_app_load_wait_interval));
+        mAppListItemManager.registerListener(notificationsAppListController);
+        mAppListItemManager.registerListener(recentNotificationsController);
+        recentNotificationsController.setApplicationsState(applicationsState);
+
+        // Since an app can show in both sections, use a listener to notify the other section to
+        // update in order to maintain consistency
+        notificationsAppListController.setNotificationSwitchListener(() ->
+                mAppListItemManager.onPackageListChanged());
+        recentNotificationsController.setNotificationSwitchListener(() ->
+                mAppListItemManager.onPackageListChanged());
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAppListItemManager.startLoading(getAppFilter(), ApplicationsState.ALPHA_COMPARATOR);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAppListItemManager.onFragmentStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAppListItemManager.onFragmentStop();
+    }
+
+    private ApplicationsState.AppFilter getAppFilter() {
+        // Display only non-system apps
+        return ApplicationsState.FILTER_DOWNLOADED_AND_LAUNCHER_AND_INSTANT;
+    }
+
+    /**
+     * Callback that is called when a app's notification setting is toggled
+     */
+    public interface NotificationSwitchListener {
+        /**
+         * An app's notification setting has been changed
+         */
+        void onSwitchChanged();
     }
 
     /**
