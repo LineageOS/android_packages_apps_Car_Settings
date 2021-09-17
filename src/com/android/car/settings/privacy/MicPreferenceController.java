aef1bb569e4b0e43f7f4097d164a7be3ea948a45
Jordan Jozwiak <jjoz@google.com>
2021-09-17 13:07:00 -0700

    Remove toggles for mic & location from privacy
    Remove the toggles from the main Settings>Privacy page, per UX
    feedback. Toggles are still available within the Microphone and
    Location subpages.
    
    Before: https://hsv.googleplex.com/5088968318124032
    After: https://hsv.googleplex.com/6314938077282304
    
    Bug: 200051708
    Bug: 182202223
    Test: atest CarSettingsUnitTests
    Change-Id: I67c01ca15b1522e0c821ede08930848099facfc1

--- src/com/android/car/settings/privacy/MicPreferenceController.java
+++ src/com/android/car/settings/privacy/MicPreferenceController.java
@@ -21,52 +21,31 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PermissionGroupInfo;
 import android.graphics.drawable.Drawable;
-import android.hardware.SensorPrivacyManager;
 
 import com.android.car.apps.common.util.Themes;
 import com.android.car.settings.R;
 import com.android.car.settings.common.FragmentController;
 import com.android.car.settings.common.Logger;
 import com.android.car.settings.common.PreferenceController;
-import com.android.car.ui.preference.CarUiTwoActionSwitchPreference;
-import com.android.internal.annotations.VisibleForTesting;
+import com.android.car.ui.preference.CarUiPreference;
 
 /** Business logic for controlling the privacy center mic setting. */
 public class MicPreferenceController
-        extends PreferenceController<CarUiTwoActionSwitchPreference> {
+        extends PreferenceController<CarUiPreference> {
     private static final Logger LOG = new Logger(MicPreferenceController.class);
     public static final String PERMISSION_GROUP_MICROPHONE = "android.permission-group.MICROPHONE";
-
-    private SensorPrivacyManager mSensorPrivacyManager;
-
-    private SensorPrivacyManager.OnSensorPrivacyChangedListener mListener =
-            new SensorPrivacyManager.OnSensorPrivacyChangedListener() {
-                @Override
-                public void onSensorPrivacyChanged(int sensor, boolean enabled) {
-                    refreshUi();
-                }
-            };
 
     private PackageManager mPm;
 
     public MicPreferenceController(Context context, String preferenceKey,
             FragmentController fragmentController, CarUxRestrictions uxRestrictions) {
-        this(context, preferenceKey, fragmentController, uxRestrictions,
-                SensorPrivacyManager.getInstance(context));
-    }
-
-    @VisibleForTesting
-    MicPreferenceController(Context context, String preferenceKey,
-            FragmentController fragmentController, CarUxRestrictions uxRestrictions,
-            SensorPrivacyManager sensorPrivacyManager) {
         super(context, preferenceKey, fragmentController, uxRestrictions);
-        mSensorPrivacyManager = sensorPrivacyManager;
         mPm = context.getPackageManager();
     }
 
     @Override
-    protected Class<CarUiTwoActionSwitchPreference> getPreferenceType() {
-        return CarUiTwoActionSwitchPreference.class;
+    protected Class<CarUiPreference> getPreferenceType() {
+        return CarUiPreference.class;
     }
 
     @Override
@@ -82,45 +61,5 @@ public class MicPreferenceController
             // fall back to using the CarSettings icon specified in the resource file
             LOG.e("Unable to load permission icon from PackageManager", e);
         }
-
-        getPreference().setOnSecondaryActionClickListener(isChecked -> {
-            // Settings UX currently shows "checked means mic is enabled", but the underlying API is
-            // inversely written around "is mic muted?" So we must be careful when doing
-            // comparisons.
-            boolean isMicMuted = mSensorPrivacyManager.isSensorPrivacyEnabled(
-                    SensorPrivacyManager.Sensors.MICROPHONE);
-            if (isChecked == isMicMuted) {
-                // UX and underlying API state for mic do not match, so update sensor privacy
-                mSensorPrivacyManager.setSensorPrivacyForProfileGroup(
-                        SensorPrivacyManager.Sources.SETTINGS,
-                        SensorPrivacyManager.Sensors.MICROPHONE,
-                        !isChecked);
-            }
-        });
-    }
-
-    @Override
-    protected void onStartInternal() {
-        mSensorPrivacyManager.addSensorPrivacyListener(
-                SensorPrivacyManager.Sensors.MICROPHONE, mListener);
-    }
-
-    @Override
-    protected void onStopInternal() {
-        mSensorPrivacyManager.removeSensorPrivacyListener(SensorPrivacyManager.Sensors.MICROPHONE,
-                mListener);
-    }
-
-    @Override
-    protected int getAvailabilityStatus() {
-        boolean hasFeatureMicToggle = mSensorPrivacyManager.supportsSensorToggle(
-                SensorPrivacyManager.Sensors.MICROPHONE);
-        return hasFeatureMicToggle ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
-    }
-
-    @Override
-    protected void updateState(CarUiTwoActionSwitchPreference preference) {
-        preference.setSecondaryActionChecked(!mSensorPrivacyManager.isSensorPrivacyEnabled(
-                SensorPrivacyManager.Sensors.MICROPHONE));
     }
 }
