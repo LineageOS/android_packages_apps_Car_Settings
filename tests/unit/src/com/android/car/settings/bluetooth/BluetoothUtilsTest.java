cf75551a4fa70513f9a0a045a835abf026030bdb
eschiang <eschiang@google.com>
2022-04-20 16:54:31 -0700

    RESTRICT AUTOMERGE Fix apps being able to turn on bluetooth scanning
    Apps are able to launch bluetooth scanning even without the
    android.permission.BLUETOOTH_ADVERTISE permission
    
    Bug: 220741473
    Test: Manual with PoC app, atest CarSettingsUnitTests:com.android.car.settings.bluetooth.BluetoothUtilsTest
    Change-Id: I5b5c38b4e9c0fac03e449008095f933e4fadea11

---
+++ tests/unit/src/com/android/car/settings/bluetooth/BluetoothUtilsTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.settings.bluetooth;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(AndroidJUnit4.class)
+public final class BluetoothUtilsTest {
+
+    private static final String SYSTEM_UI_PACKAGE_NAME = "com.package.systemui";
+    private static final String SYSTEM_UI_COMPONENT_NAME = "com.package.systemui/testclass";
+    private final Context mContext = spy(ApplicationProvider.getApplicationContext());
+
+    @Mock
+    private Resources mMockResources;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(mContext.getResources()).thenReturn(mMockResources);
+        when(mMockResources.getString(anyInt())).thenReturn(SYSTEM_UI_COMPONENT_NAME);
+    }
+
+    @Test
+    public void isSystemCallingPackage_shouldEnableBluetoothScanning() {
+        String settingsPackage = mContext.getPackageName();
+
+        assertThat(BluetoothUtils.shouldEnableBTScanning(mContext, settingsPackage))
+                .isEqualTo(true);
+        assertThat(BluetoothUtils.shouldEnableBTScanning(mContext, SYSTEM_UI_PACKAGE_NAME))
+                .isEqualTo(true);
+    }
+
+    @Test
+    public void isNotSystemCallingPackage_shouldNotEnableBluetoothScanning() {
+        String fakePackage = "not.real.package";
+
+        assertThat(BluetoothUtils.shouldEnableBTScanning(mContext, fakePackage))
+                .isEqualTo(false);
+    }
+}
