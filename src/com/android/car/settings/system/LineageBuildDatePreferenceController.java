1787a433421498a80ac99737a7ada6ce2a299b4d
Bruno Martins <bgcngm@gmail.com>
2024-01-22 10:42:59 +0000

    CarSettings: Add LineageOS entries into About screen
    Change-Id: If5059d7c687f8831763a26a87718bbbd9358d998

---
+++ src/com/android/car/settings/system/LineageBuildDatePreferenceController.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.settings.system;
+
+import android.car.drivingstate.CarUxRestrictions;
+import android.content.Context;
+import android.os.SystemProperties;
+
+import androidx.preference.Preference;
+
+import com.android.car.settings.R;
+import com.android.car.settings.common.FragmentController;
+import com.android.car.settings.common.PreferenceController;
+
+public class LineageBuildDatePreferenceController extends PreferenceController<Preference> {
+
+    private static final String KEY_BUILD_DATE_PROP = "ro.build.date";
+
+    public LineageBuildDatePreferenceController(Context context, String preferenceKey,
+            FragmentController fragmentController, CarUxRestrictions uxRestrictions) {
+        super(context, preferenceKey, fragmentController, uxRestrictions);
+    }
+
+    @Override
+    protected Class<Preference> getPreferenceType() {
+        return Preference.class;
+    }
+
+    @Override
+    protected void updateState(Preference preference) {
+        preference.setSummary(SystemProperties.get(KEY_BUILD_DATE_PROP,
+                getContext().getString(R.string.unknown)));
+    }
+
+    @Override
+    protected int getDefaultAvailabilityStatus() {
+        return AVAILABLE_FOR_VIEWING;
+    }
+}
